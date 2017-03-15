ae6ff102f021363f4bd18223fad3d3df045ad725
davi7725 <davi7725@edu.eal.dk>
2017-03-15 14:51:09 +0100

    U02 tests and code implemented

---
+++ FynbusProject/CSVImport.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FynbusProject
+{
+    public static class CSVImport
+    {
+        public static bool Import(string filepath)
+        {
+            bool importSucessful;
+            string typeOfFile = CheckTypeOfFile(filepath);
+            if(typeOfFile == "Offer")
+            {
+                importSucessful = true;
+            }
+            else
+            {
+                importSucessful = false;
+            }
+            return importSucessful;
+        }
+
+        private static string CheckTypeOfFile(string filepath)
+        {
+            string fileType = "";
+            if(filepath.EndsWith("Tilbud_Skabelon.csv"))
+            {
+                fileType = "Offer";
+            }
+            else if(filepath.EndsWith("Stamoplysninger_Skabelon.csv"))
+            {
+                fileType = "Contractors";
+            }
+
+            return fileType;
+        }
+    }
+}
---
+++ FynbusTests/ImportTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FynbusProject;
+
+namespace FynbusTests
+{
+    [TestClass]
+    public class ImportTests
+    {
+        [TestMethod]
+        public void ImportOfferFile()
+        {
+            string contractorFilepath = @"C:\Users\Kast\Desktop\Fynbus\Flexcel_Fynbus\FakeData_Tests\Stamoplysninger_Skabelon.csv";
+            bool contractorFile = CSVImport.Import(contractorFilepath);
+
+            string offerFilepath = @"C:\Users\Kast\Desktop\Fynbus\Flexcel_Fynbus\FakeData_Tests\Tilbud_Skabelon.csv";
+            bool offerFile = CSVImport.Import(offerFilepath);
+
+            Assert.IsFalse(contractorFile);
+            Assert.IsTrue(offerFile);
+        }
+    }
+}
