c302d6d0319ef9a9177f69c7258fb223cd5f4937
davi7725 <davi7725@edu.eal.dk>
2017-03-16 12:21:40 +0100

    Create the contractor, offer and route classes to match the changes in the CD.

---
+++ FynbusProject/Offer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FynbusProject
+{
+    class Offer
+    {
+        private int _id;
+        private int _vehicleType;
+        private int _availableHours;
+
+        public Offer(int id, int vehType, int avaHours)
+        {
+            _id = id;
+            _vehicleType = vehType;
+            _availableHours = avaHours;
+        }
+    }
+}
---
+++ FynbusProject/Route.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FynbusProject
+{
+    class Route
+    {
+        private int _routeNumber;
+        private int _vehicleType;
+
+        public Route(int routeNb, int vehType)
+        {
+            _routeNumber = routeNb;
+            _vehicleType = vehType;
+        }
+    }
+}
