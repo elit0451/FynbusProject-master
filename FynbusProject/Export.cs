915cc64767699bae53ffbaf1b1311bc94c75e2af
alex855k <alexander2341@gmail.com>
2017-03-30 14:31:14 +0200

    Small changes

---
+++ FynbusProject/Export.cs
@@ -0,0 +1,24 @@
+﻿namespace FynbusProject
+{
+    public class Export
+    {
+        private CalculateWinner cw = null;
+        public Export(CalculateWinner calculateWinner)
+        {
+            cw = calculateWinner;
+        }
+
+        public void ExportToPDF()
+        {
+
+        }
+
+        public void ExportToCVS()
+        {
+
+        }
+
+
+
+    }
+}
